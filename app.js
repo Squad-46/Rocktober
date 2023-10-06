a5bded3c8845f506054183c8bffc73c76994b37e
Kavin0M <kavinsai88@gmail.com>
2023-10-06 14:53:05 +0530

    Added form links

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 document.getElementById("kalvium").addEventListener("click",() => {
     window.open("https://kalvium.com/","_self")
 })
+
+document.getElementById("btn1").addEventListener("click",() => {
+    window.open("https://forms.gle/atLsbroUyDpPTYMC7","_self")
+})
+
+document.getElementById("btn2").addEventListener("click",() => {
+    window.open("https://forms.gle/vHTmMxmRrapXtKSdA","_self")
+})
+
+document.getElementById("btn3").addEventListener("click",() => {
+    window.open("https://forms.gle/Dg2DzEbLHfQoeCgg9","_self")
+})
