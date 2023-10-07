cea41e9e8297f9d92d0cc7b54ec16d45f072f142
vedha <vedh1305@gmail.com>
2023-10-07 13:53:28 +0530

    Rocktober

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ document.getElementById("btn1").addEventListener("click",() => {
 })
 
 document.getElementById("btn2").addEventListener("click",() => {
-    window.open("https://forms.gle/vHTmMxmRrapXtKSdA","_self")
+    window.open("https://forms.gle/ittWbD54A2B2MmKr5","_self")
 })
 
 document.getElementById("btn3").addEventListener("click",() => {
     window.open("https://forms.gle/Dg2DzEbLHfQoeCgg9","_self")
+})
+document.getElementById("btn4").addEventListener("click",() => {
+    window.open("https://forms.gle/wB9B1KdMcqpPWq1KA","_self")
 })
