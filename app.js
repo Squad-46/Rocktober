9ea2b662832870a782e7c41e37de2430cc7307d6
vedha <vedh1305@gmail.com>
2023-10-19 16:21:27 +0530

    update

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 document.getElementById("kalvium").addEventListener("click",() => {
     window.open("https://kalvium.com/","_self")
 })
-
 document.getElementById("btn1").addEventListener("click",() => {
-    window.open("https://forms.gle/atLsbroUyDpPTYMC7","_self")
+    window.open("https://forms.gle/cMJ6UuRGmVT5HTS48","_self")
 })
-
 document.getElementById("btn2").addEventListener("click",() => {
-    window.open("https://forms.gle/ittWbD54A2B2MmKr5","_self")
-})
-
-document.getElementById("btn3").addEventListener("click",() => {
     window.open("https://forms.gle/Dg2DzEbLHfQoeCgg9","_self")
 })
-document.getElementById("btn4").addEventListener("click",() => {
+document.getElementById("btn3").addEventListener("click",() => {
     window.open("https://forms.gle/wB9B1KdMcqpPWq1KA","_self")
+})
+document.getElementById("btn4").addEventListener("click",() => {
+    window.open("https://forms.gle/atLsbroUyDpPTYMC7","_self")
 })
